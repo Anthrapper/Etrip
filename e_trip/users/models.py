45b0064bc89222179eba5eeaf41498015b191a33
Aju Ajmal <dev@ajmalaju.com>
2020-11-11 02:25:06 +0530

    add phone-number #7

--- e_trip/users/models.py
+++ e_trip/users/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.contrib.gis.db import models as geo_models
 
 
 class User(AbstractUser):
@@ -9,6 +11,7 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    phone = models.CharField(blank=True,max_length=10)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
