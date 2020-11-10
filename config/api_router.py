b878755d40cd650f5fe0117d03004d577d366e99
Aju Ajmal <dev@ajmalaju.com>
2020-11-11 02:24:15 +0530

    add jwt #8

--- config/api_router.py
+++ config/api_router.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from e_trip.users.api.views import UserViewSet
-
+from rest_framework_simplejwt import views as jwt_views
+from django.urls import path
 if settings.DEBUG:
     router = DefaultRouter()
 else:
@@ -11,5 +12,11 @@ else:
 router.register("users", UserViewSet)
 
 
+urlpatterns = [
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    #path('user/password/token/', PasswordResetAPI.as_view()),
+]
+
 app_name = "api"
-urlpatterns = router.urls
+urlpatterns += router.urls
