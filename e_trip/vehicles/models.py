908b850a1e120b151711512692126fa1d70d307d
Aju Ajmal <dev@ajmalaju.com>
2020-11-14 18:26:34 +0530

    add vehicles models #5

--- e_trip/vehicles/models.py
+++ e_trip/vehicles/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+import os
+from django.utils import timezone
+from datetime import datetime
+
+def material_directory_path(instance, filename):
+    basefilename, file_extension= os.path.splitext(filename)
+    timenow = timezone.now()
+    return 'assets/icons/{basename}{time}{ext}'.format( basename=basefilename, time=timenow.strftime("%Y%m%d%H%M%S"), ext=file_extension)
+
+
 class Vehicle(models.Model):
     name = models.CharField(max_length=10)
-    icon = models.URLField()
+    icon = models.FileField(upload_to=material_directory_path, default='file.pdf',help_text="Display Icon")
+    #icon = models.URLField()
+    def __str__(self):
+        return self.name
