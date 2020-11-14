8c3f1bc0caec6e537a9884d99d7976a4c74642d3
Aju Ajmal <dev@ajmalaju.com>
2020-11-14 18:25:05 +0530

    update firebase auth-cred #5

--- e_trip/users/api/views.py
+++ e_trip/users/api/views.py
@@ -34,7 +34,7 @@ regex_mail = settings.EMAIL_REGEX
 
 if not firebase_admin._apps:
     cred = credentials.Certificate(settings.GOOGLE_APPLICATION_CREDENTIALS)
-    default_app = firebase_admin.initialize_app(cred)
+    default_app = firebase_admin.initialize_app(cred,config)
 
 firebase = pyrebase.initialize_app(config)
 
