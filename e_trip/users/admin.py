272bfa7fe17cd35df0fdece47903a972dc4ac550
Aju Ajmal <dev@ajmalaju.com>
2020-11-15 00:37:20 +0530

    update driver model admin view #7

--- e_trip/users/admin.py
+++ e_trip/users/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
 from e_trip.users.forms import UserChangeForm, UserCreationForm
-
+from .models import Driver
 User = get_user_model()
 
 
@@ -17,6 +17,8 @@ class UserAdmin(auth_admin.UserAdmin):
     )
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
+
+admin.site.register(Driver)
 
 admin.site.site_header = "ETrip Admin"
 admin.site.site_title = "ETrip Dashboard"
