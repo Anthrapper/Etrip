cd10f9b44a7052458d258fa1c2d8a3364a1af0ee
Aju Ajmal <dev@ajmalaju.com>
2020-11-16 00:57:31 +0530

    update db engine #21

--- config/settings/production.py
+++ config/settings/production.py
@@ -195,8 +195,9 @@ sentry_sdk.init(
 
 # GDAL
 # ------------------------------------------------------------------------------
-GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GDAL_LIBRARY_PATH')
+#GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GEOS_LIBRARY_PATH')
+#GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GDAL_LIBRARY_PATH')
 
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+DATABASES["default"]["ENGINE"] = 'django.contrib.gis.db.backends.postgis'
