7e48e0f061a0c67281d1e32bcbcb163f2eb6ede2
Aju Ajmal <dev@ajmalaju.com>
2020-11-16 10:09:26 +0530

    gis config for #21

--- env.sh
+++ env.sh
@@ -2,3 +2,5 @@ export DATABASE_URL="sqlite:///db.sqlite"
 export CELERY_BROKER_URL=
 export USE_DOCKER=False
 export GOOGLE_APPLICATION_CREDENTIALS="admin.json"
+
+export DATABASE_URL="postgis://user001:123456789@localhost:5433/gis"
