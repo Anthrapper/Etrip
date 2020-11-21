ca968cefa979c4f8e46d3806971f797afd3c568b
Aju Ajmal <dev@ajmalaju.com>
2020-11-21 20:46:26 +0530

    add parsers in #27

--- e_trip/vehicles/api/views.py
+++ e_trip/vehicles/api/views.py
@@ -10,6 +10,7 @@ from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
+from rest_framework.parsers import MultiPartParser, FormParser
 from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView ,UpdateAPIView
 from .serializers import VehicleSerializer, DriverVehicleSerializer, DriverVehicleListSerializer
@@ -22,6 +23,7 @@ class VehicleList(generics.ListAPIView):
 
 class CreateDriverVehicle(CreateAPIView):
     serializer_class = DriverVehicleSerializer
+    parser_classes = (MultiPartParser, FormParser)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
