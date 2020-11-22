fa548df702415a657596181fd5183db2fd0d1b49
Aju Ajmal <dev@ajmalaju.com>
2020-11-23 03:55:31 +0530

    add vehicle image in bid logs #42

--- e_trip/trips/api/serializers.py
+++ e_trip/trips/api/serializers.py
@@ -10,6 +10,7 @@ from botocore.client import Config
 
 from e_trip.trips.models import Trip, Bid
 from e_trip.users.models import Driver
+from e_trip.vehicles.models import DriverVehicle
 
 class UserTripCreateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -78,14 +79,53 @@ class DriverBidCreateSerializer(serializers.ModelSerializer):
         driver_bid.save()
         return driver_bid
 
+
+logger = logging.getLogger('django')
+
+def create_presigned_url(bucket_name, object_name, expiration=3600):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+
+    # Generate a presigned URL for the S3 object
+    s3_client = boto3.client('s3',
+                      config=Config(signature_version='s3v4'),
+                      aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                      aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                      region_name=settings.AWS_S3_REGION_NAME
+    )
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL
+    return response
+
 class BidLogSerializer(serializers.ModelSerializer):
     driver = serializers.SerializerMethodField()
+    vehicle = serializers.SerializerMethodField()
     class Meta:
         model = Bid
         fields = '__all__'
     def get_driver(self,obj):
         return obj.driver.user.name
-
+    def get_vehicle(self,obj):
+        driver = obj.driver
+        trip = get_object_or_404(Trip, id=obj.trip.id)
+        vehicle = DriverVehicle.objects.filter(vehicle = trip.vehicle).first()
+        url = create_presigned_url(settings.AWS_STORAGE_BUCKET_NAME, 'media/' + vehicle.photo.name)
+        res = requests.get(url)
+        return res.url
+        
 class TripBidSelectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bid
