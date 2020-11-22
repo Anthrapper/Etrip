b3858c5ac9fc79957b7a0da7df4d28e782db0614
Aju Ajmal <dev@ajmalaju.com>
2020-11-22 06:49:54 +0530

    create bid model #15 update #13

--- e_trip/trips/models.py
+++ e_trip/trips/models.py
@@ -13,7 +13,7 @@ class Trip(models.Model):
     to_place = models.CharField(blank=True,max_length=200)
     user = models.ForeignKey('users.User',on_delete=models.CASCADE,blank=True,null=True)
     driver = models.ForeignKey('users.Driver',on_delete=models.CASCADE,blank=True, null=True)
-    amount = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    amount = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
     vehicle = models.ForeignKey('vehicles.Vehicle',on_delete=models.CASCADE,blank=True,null=True)
     TYPE_VALUE_MAP = {
         "OnBid":0,
@@ -26,3 +26,19 @@ class Trip(models.Model):
     trip_status = models.IntegerField(choices=TYPE_CHOICES, blank=True, default=0)
     def __str__(self):
         return " " + self.from_place + " to " + self.to_place
+
+class Bid(models.Model):
+    trip = models.ForeignKey('Trip',on_delete=models.CASCADE,blank=True,null=True)
+    driver = models.ForeignKey('users.Driver',on_delete=models.CASCADE,blank=True, null=True)
+    amount = models.DecimalField(max_digits=8, decimal_places=2, null=True,blank=True)
+    date =  models.DateTimeField(default=timezone.now)
+    TYPE_VALUE_MAP = {
+        "None":0,
+        "Applied": 1,
+        "Approved": 2,
+        "Cancelled": 3
+        }
+    TYPE_CHOICES = [(value, name) for name, value in TYPE_VALUE_MAP.items()]
+    bid_status = models.IntegerField(choices=TYPE_CHOICES, blank=True, default=0)
+    def __str__(self):
+        return " " + self.trip.from_place + " to " + self.trip.to_place
